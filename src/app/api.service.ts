47460d4610287902b8b29c08932aa319cc8e6d40
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-08 07:09:51 +0530

    api

---
+++ src/app/api.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  fetchProduct=()=>
+  {
+    return this.http.get("http://localhost:8080/view")
+  }
+  addProduct=(dataToSend:any)=>
+  {
+    return this.http.post("http://localhost:8080/search",dataToSend)
+  }
+  searchProduct=(dataToSend:any)=>
+  {
+    return this.http.post("http://localhost:8080/search",dataToSend)
+  }
+  addUser=(dataToSend:any)=>
+  {
+    return this.http.post("http://localhost:8080/register",dataToSend)
+  }
+  
+}
