b5cb8527855373c4528e3d811d127ccb6ad5443e
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-11 17:45:56 +0530

    getuserbyid

--- src/app/view-profile/view-profile.component.ts
+++ src/app/view-profile/view-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-view-profile',
@@ -6,8 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-profile.component.css']
 })
 export class ViewProfileComponent {
-  userId:any = ""
-  constructor() {
+  userId: any = ""
+  constructor(private api: ApiService) {
     this.userId = localStorage.getItem("userInfo")
+    let data: any = {
+      "id": this.userId
+    }
+    this.api.getprofileDetails(data).subscribe(
+      (response: any) => {
+        console.log(response)
+        this.data=response
+      }
+    )
   }
+  data:any=[]
 }
